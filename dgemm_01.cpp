e1919abefbb85c6ab602a3e6bf43b5b1f9d35150
NAKATA Maho <maho.nakata@gmail.com>
2025-02-05 18:37:24 +0900

    .

---
+++ dgemm_01.cpp
@@ -0,0 +1,18 @@
+void dgemm(int M, int N, int K, double alpha, const double *A, int lda, const double *B, int ldb, double beta, double *C, int ldc) {
+
+    //まずCをbeta倍する
+    for (int j = 0; j < N; j++) {
+        // C の j 列を beta 倍する
+        for (int i = 0; i < M; i++) {
+            C[i + j * ldc] *= beta;
+        }
+    }
+    //Cij <- αＡikBkj + βij
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < K; k++) {
+                C[i + j * ldc] += temp * A[i + k * lda] * B[k + j * ldb];
+            }
+        }
+    }
+}
