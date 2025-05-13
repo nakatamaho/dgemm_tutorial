cac2383d8209dd71642110532217d5bcd2366915
Nakata Maho <maho.nakata@gmail.com>
2025-05-13 18:57:14 +0900

    .

--- 17/go.sh
+++ 17/go.sh
@@ -5,4 +5,5 @@ g++ -O3 -march=native 17_dgemm_4x8avx2_kernel.cpp    -o 17_dgemm_4x8avx2_kernel 
 g++ -O3 -march=native 17_dgemm_6x8avx2_kernel.cpp    -o 17_dgemm_6x8avx2_kernel  ; ./17_dgemm_6x8avx2_kernel   --nocheck
 g++ -O3 -march=native 17_dgemm_8x4avx2_kernel.cpp    -o 17_dgemm_8x4avx2_kernel  ; ./17_dgemm_8x4avx2_kernel   --nocheck
 g++ -O3 -march=native 17_dgemm_8x6avx2_kernel.cpp    -o 17_dgemm_8x6avx2_kernel  ; ./17_dgemm_8x6avx2_kernel   --nocheck
+g++ -O3 -march=native 17_dgemm_8x8avx2_kernel.cpp    -o 17_dgemm_8x8avx2_kernel  ; ./17_dgemm_8x8avx2_kernel   --nocheck
 #python3 plot.py
