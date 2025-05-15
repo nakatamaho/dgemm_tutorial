48677944b6f2ae1fcafadbd288d049cb01354b4d
Nakata Maho <maho.nakata@gmail.com>
2025-05-15 12:20:17 +0900

    .

--- 17/go.sh
+++ 17/go.sh
@@ -1,6 +1,6 @@
 cd /home/maho/dgemm_tutorial/17 ; \
 g++ -O3 -march=native -funroll-loops 17_dgemm_4x12avx2_kernel.cpp   -o 17_dgemm_4x12avx2_kernel ; ./17_dgemm_4x12avx2_kernel  --nocheck
-g++ -O3 -march=native -funroll-loops 17_dgemm_4x12avx2_kernel.2.cpp -o 17_dgemm_4x12avx2_kernel.2 ; ./17_dgemm_4x12avx2_kernel.2  --nocheck
+g++ -O3 -march=znver2 -mtune=znver2 -ffast-math -funroll-loops -flto 17_dgemm_4x12avx2_kernel.2.cpp -o 17_dgemm_4x12avx2_kernel.2 ; ./17_dgemm_4x12avx2_kernel.2  --nocheck
 g++ -O3 -march=native -funroll-loops 17_dgemm_4x4avx2_kernel.cpp    -o 17_dgemm_4x4avx2_kernel  ; ./17_dgemm_4x4avx2_kernel   --nocheck
 g++ -O3 -march=native -funroll-loops 17_dgemm_4x8avx2_kernel.cpp    -o 17_dgemm_4x8avx2_kernel  ; ./17_dgemm_4x8avx2_kernel   --nocheck
 g++ -O3 -march=native -funroll-loops 17_dgemm_6x8avx2_kernel.cpp    -o 17_dgemm_6x8avx2_kernel  ; ./17_dgemm_6x8avx2_kernel   --nocheck
@@ -13,6 +13,7 @@ g++ -O3 -march=native 17_dgemm_openblas.cpp  -o 17_dgemm_openblas ../07/OpenBLAS
 python3 plot.py \
   dgemm_benchmark_12x4avx2_kernel_results.csv \
   dgemm_benchmark_4x12avx2_kernel_results.csv \
+  dgemm_benchmark_4x12avx2_kernel_results.2.csv \
   dgemm_benchmark_4x4avx2_kernel_results.csv \
   dgemm_benchmark_4x8avx2_kernel_results.csv \
   dgemm_benchmark_6x8avx2_kernel_results.csv \
